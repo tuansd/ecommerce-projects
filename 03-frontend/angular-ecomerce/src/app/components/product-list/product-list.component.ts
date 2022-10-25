d6952d22f22cddf5ae617afd3ce1ada5187f62f1
Tuan Tran <hometran@gmail.com>
2022-10-25 11:27:31 -0700

    updated Angular Component

--- 03-frontend/angular-ecomerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecomerce/src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list-table.component.html',
+  // templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.listProducts();
   }
-
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Build a Full Stack E-Commerce website with Angular and Java Spring Boot
 (includes Stripe Credit Card Payments)
+
+image.png
